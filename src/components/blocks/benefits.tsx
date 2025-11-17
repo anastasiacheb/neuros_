957c150e8fceb1fecdc9070bef2d908e302bfa9d
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-11-17 08:21:26 +0300

    fix

--- src/components/blocks/benefits.tsx
+++ src/components/blocks/benefits.tsx
@@ -4,19 +4,19 @@ import { PAGE_QUERYResult } from '@/sanity/types';
 const BenefitsData = [
   {
     srcLight: '/icons/credit_card_cursor_click.svg',
-    srcDark: '/icons/credit_card_cursor_clickdark.svg',
+    srcDark: '/icons/credit_card_cursor_click.svg',
     title: 'Instant Access',
     text: "Begin exploring Neuros's full suite of features immediately",
   },
   {
     srcLight: '/icons/chart_rates_checkmark1.svg',
-    srcDark: '/icons/chart_rates_checkmark1dark.svg',
+    srcDark: '/icons/chart_rates_checkmark1.svg',
     title: 'No Commitments',
     text: 'No credit card required, and you can cancel anytime',
   },
   {
     srcLight: '/icons/World,_Transaction,_Transfer.svg',
-    srcDark: '/icons/World,_Transaction,_Transferdark.svg',
+    srcDark: '/icons/World,_Transaction,_Transfer.svg',
     title: 'Dedicated Support',
     text: 'Our team is here to assist you every step of the way during your trial.',
   },
@@ -31,16 +31,16 @@ interface BenefitItemProps {
 
 function BenefitItem({ srcLight, srcDark, title, text }: BenefitItemProps) {
   return (
-    <div className="bg-linear-to-br from-primary-0/10 to-primary-0/15 backdrop-blur-2 rounded-2xl border border-primary-0/20 p-2 flex gap-3">
+    <div className="bg-linear-to-br from-primary-01/10 to-primary-01/15 backdrop-blur-2 rounded-2xl border border-primary-01/20 p-2 flex gap-3">
       <ThemeImage
         srcLight={srcLight}
         srcDark={srcDark}
         alt="decor"
-        className="bg-primary-0/10 rounded-xl px-3 py-6 w-12 h-18"
+        className="bg-primary-01/10 rounded-xl px-3 py-6 w-12 h-18"
       />
       <div>
-        <h3 className="font-medium text-base text-primary-0">{title}</h3>
-        <p className="text-base text-primary-0/70">{text}</p>
+        <h3 className="font-medium text-base text-primary-01">{title}</h3>
+        <p className="text-base text-primary-01/70">{text}</p>
       </div>
     </div>
   );
@@ -50,24 +50,19 @@ type BenefitsProps = Extract<NonNullable<NonNullable<PAGE_QUERYResult>['content'
 
 export function Benefits({ title }: BenefitsProps) {
   return (
-    <section className="max-w-360 lg:px-28 lg:mx-auto">
+    <section className="max-w-360 lg:px-28 lg:mx-auto ">
       <div className="bg-accent-600 pt-8 pb-14 px-8 my-14 md:rounded-4xl md:p-14 md:my-20 md:mx-8 lg:max-w-301 lg:mx-auto flex flex-col gap-10 lg:flex-row relative overflow-hidden">
         <ThemeImage
-          srcDark="/images/Mask_groupdark.png"
+          srcDark="/images/Mask_group.png"
           srcLight="/images/Mask_group.png"
           alt="play"
           className="w-full h-auto lg:h-full lg:w-auto absolute object-cover bottom-0 left-0"
         />
         <div className="flex-1 relative z-10">
-          <h2 className="font-DM font-bold text-4xl mb-10 md:text-5xl text-primary-0">{title}</h2>
-          <button className="font-DM font-bold text-base text-primary-0 bg-linear-to-br from-primary-0/10 to-primary-0/25 backdrop-blur-2 rounded-xl py-4 px-8 w-full md:w-auto flex gap-2 justify-center">
+          <h2 className="font-DM font-bold text-4xl mb-10 md:text-5xl text-primary-01">{title}</h2>
+          <button className="font-DM font-bold text-base text-primary-01 bg-linear-to-br from-primary-01/10 to-primary-01/25 backdrop-blur-2 rounded-xl py-4 px-8 w-full md:w-auto flex gap-2 justify-center">
             Start your free trial
-            <ThemeImage
-              srcLight="/icons/staricon.svg"
-              srcDark="/icons/staricondark.svg"
-              alt="decor"
-              className="w-6 h-6"
-            />
+            <ThemeImage srcLight="/icons/staricon.svg" srcDark="/icons/staricon.svg" alt="decor" className="w-6 h-6" />
           </button>
         </div>
         <div className="flex flex-col gap-5 flex-1 lg:max-w-95 relative z-10">
